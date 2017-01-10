6d49bdd36e63d472f7251551e09aa0dbd9f83b7b
Donovan An <donovantaean@yahoo.com>
2017-01-10 14:11:19 -0800

    completed boxing/casting challenge

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ConsoleApplication
 {
@@ -6,7 +7,33 @@ namespace ConsoleApplication
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<Object> boxes = new List<Object>();
+
+            boxes.Add(7);
+            boxes.Add(28);
+            boxes.Add(-1);
+            boxes.Add(true);
+
+            int boxSum = 0;
+            for (var idx = 0; idx < boxes.Count; idx++){
+                if (boxes[idx] is int){
+                    boxSum = boxSum + (int) boxes[idx];
+                }
+            }
+
+            Console.WriteLine(boxSum);
+
+
+            //Box some string data into a variable
+            object BoxedData = "This is clearly a string";
+            //Make sure it is the type you need before proceeding
+            if (BoxedData is int) {
+                //This shouldn't run
+                Console.WriteLine("I guess we have an integer value in this box?");
+            }
+            if (BoxedData is string) {
+                Console.WriteLine("It is totally a string in the box!");
+            }
         }
     }
 }
